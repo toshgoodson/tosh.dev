a928b3efea3741f067201b2fc34c7cde928fa47e
Toshiro Goodson <tosh.goodson@gmail.com>
2023-02-23 21:22:31 -0800

    non-breaking hyphen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
 				<div className="text-stone-100 rfs:text-[80px] font-extrabold leading-tight">Tosh Goodson.</div>
 			</h1>
 			<h2 className="md:rfs:text-[80px] rfs:text-[60px] font-extrabold leading-tight mb-4">Coding is what I love.</h2>
-			<div className="md:max-w-[70%] leading-relaxed">I&apos;m a full stack software engineer focused on building robust and performant web apps. Presently, I&apos;m working on e-commerce shopping experiences at <Link className="text-amber-300 underline" href="https://benefytt.com/" target="_blank">Benefytt.com</Link>.</div>
+			<div className="md:max-w-[70%] leading-relaxed">I&apos;m a full stack software engineer focused on building robust and performant web apps. Presently, I&apos;m working on e&#8209;commerce shopping experiences at <Link className="text-amber-300 underline" href="https://benefytt.com/" target="_blank">Benefytt.com</Link>.</div>
 		</main>
 	)
 }
