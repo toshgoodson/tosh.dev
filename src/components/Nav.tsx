f1318122c2aee276ca23cfc6f30f5f7ec9148dc0
Toshiro Goodson <tosh.goodson@gmail.com>
2023-02-26 16:15:40 -0800

    flicker animation for logo

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -14,14 +14,14 @@ export const Nav: FC = () => {
 			<div className="bg-inherit sticky top-[env(safe-area-inset-top, 0px)] z-50 h-16">
 				<div className="container mx-auto max-w-4xl px-4 py-4 flex items-center">
 					<Link href="/" className="font-pixel text-4xl group">
-						<span className={`${logoTransition}               group-hover:delay-[300ms] group-hover:durartion-300`}>t</span>
-						<span className={`${logoTransition} delay-[25ms]  group-hover:delay-[800ms]`}>o</span>
-						<span className={`${logoTransition} delay-[50ms]  group-hover:delay-[1500ms] group-hover:duration-300`}>s</span>
-						<span className={`${logoTransition} delay-75      group-hover:delay-[2200ms]`}>h</span>
-						<span className={`${logoTransition} delay-100     group-hover:delay-[2600ms] text-amber-300`}>.</span>
-						<span className={`${logoTransition} delay-[125ms] group-hover:delay-[2900ms]`}>d</span>
-						<span className={`${logoTransition} delay-150     group-hover:delay-[3400ms] group-hover:duration-300`}>e</span>
-						<span className={`${logoTransition} delay-[175ms] group-hover:delay-[4400ms]`}>v</span>
+						<span className={`${logoTransition}               group-hover:delay-[300ms]                           group-hover:animate-flicker group-hover:[animation-delay:300ms]`}>t</span>
+						<span className={`${logoTransition} delay-[25ms]  group-hover:delay-[800ms]                           group-hover:animate-flicker group-hover:[animation-delay:800ms]`}>o</span>
+						<span className={`${logoTransition} delay-[50ms]  group-hover:delay-[1500ms] group-hover:duration-300 group-hover:animate-flicker group-hover:[animation-delay:1500ms]`}>s</span>
+						<span className={`${logoTransition} delay-75      group-hover:delay-[2200ms]                          group-hover:animate-flicker group-hover:[animation-delay:2200ms]`}>h</span>
+						<span className={`${logoTransition} delay-100     group-hover:delay-[2600ms]                          group-hover:animate-flicker group-hover:[animation-delay:2600ms] text-amber-300`}>.</span>
+						<span className={`${logoTransition} delay-[125ms] group-hover:delay-[2900ms]                          group-hover:animate-flicker group-hover:[animation-delay:2900ms]`}>d</span>
+						<span className={`${logoTransition} delay-150     group-hover:delay-[3400ms] group-hover:duration-300 group-hover:animate-flicker group-hover:[animation-delay:3400ms]`}>e</span>
+						<span className={`${logoTransition} delay-[175ms] group-hover:delay-[4400ms]                          group-hover:animate-flicker group-hover:[animation-delay:4400ms]`}>v</span>
 					</Link>
 					<div className="flex-grow"></div>
 					<Button as="a" className="text-base px-3 py-1" href="/resume">Résumé</Button>
