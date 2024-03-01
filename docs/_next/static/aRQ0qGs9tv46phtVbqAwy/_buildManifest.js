c5786a90f048f5a0a4f2cc21c752e8cd6b89859c
Tosh Goodson <tosh.goodson@gmail.com>
2024-03-01 11:57:04 -0800

    release; use redirecting resume link

--- docs/_next/static/aRQ0qGs9tv46phtVbqAwy/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-895c6fc5795b7a41.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
